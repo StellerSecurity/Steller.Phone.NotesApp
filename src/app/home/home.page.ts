faa42ab5b87235c96b2b9c6ed0567ad66a6f334b
karimgtc <128481132+karimgtc@users.noreply.github.com>
2023-08-17 01:55:30 +0200

    Notes App.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,8 +1,12 @@
 import { Component, inject } from '@angular/core';
-import { RefresherCustomEvent } from '@ionic/angular';
-import { MessageComponent } from '../message/message.component';
+import {AlertController, RefresherCustomEvent} from '@ionic/angular';
 
-import { DataService, Message } from '../services/data.service';
+import {Router} from "@angular/router";
+import {CryptoService} from "../services/crypto.service";
+import {NotesService} from "../services/notes.service";
+declare var require: any;
+var CryptoJS = require('crypto-js');
+
 
 @Component({
   selector: 'app-home',
@@ -10,16 +14,97 @@ import { DataService, Message } from '../services/data.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  private data = inject(DataService);
-  constructor() {}
+  private notes: any;
 
-  refresh(ev: any) {
-    setTimeout(() => {
-      (ev as RefresherCustomEvent).detail.complete();
-    }, 3000);
+
+  constructor(private cryptoService: CryptoService,
+              private alertCtrl: AlertController,
+              private noteService: NotesService) {
+
+    if(this.noteService.getDecryptedNotes() === null) {
+      this.askForNotesAppPassword();
+    }
   }
 
-  getMessages(): Message[] {
-    return this.data.getMessages();
+  ionViewWillEnter() {
+
   }
+
+  private setData(password: string) {
+    let notes = this.noteService.getNotes();
+    let decryptedNotes = this.cryptoService.decrypt(notes, password);
+    this.noteService.setDecryptedNotes(decryptedNotes);
+    this.notes = JSON.parse(decryptedNotes);
+  }
+
+  /**
+   * The method will ask the password for the notes-app (if set),
+   * when the state of notes-app is either first-time opened
+   * or if have been idle for x minutes.
+   */
+  public async askForNotesAppPassword() {
+
+    // @ts-ignore
+    let alert = await this.alertCtrl.create({
+      header: 'Protected Notes App',
+      subHeader: 'Enter Password For The Notes App',
+      inputs: [
+        {
+          name: 'password',
+          placeholder: 'Password',
+          type: 'password',
+        }
+      ],
+      buttons: [
+        {
+          text: 'Reset Password',
+          role: 'cancel',
+          handler: () => {
+            //this.back();
+          },
+        },
+        {
+          text: 'Okay',
+          handler: (data: any) => {
+            // @ts-ignore
+
+            this.setData(data.password);
+
+            if(this.notes === null) {
+              // wrong password, call a counter, [where should be stored?]
+              return false;
+            }
+
+            // set counter to, 0.
+            return true;
+
+          },
+        },
+      ]
+    });
+    await alert.present();
+
+  }
+
+  /**
+   * Will get the decrypted notes (if there is any),
+   * and sort them by last modified.
+   */
+  getNotes()  {
+
+    if(this.notes === undefined) {
+      return [];
+    }
+
+    // sort notes by last modified date
+    // @ts-ignore
+    this.notes = this.notes.sort((a, b) => {
+      if (a.last_modified > b.last_modified) {
+        return -1;
+      }
+    });
+
+    return this.notes;
+  }
+
 }
---
+++ src/app/services/crypto.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+declare var require: any;
+var CryptoJS = require('crypto-js');
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CryptoService {
+
+  constructor() { }
+
+  public encrypt(value: any, password: string) {
+    let encrypted = CryptoJS.AES.encrypt(value, password);
+    return encrypted;
+  }
+
+  public decrypt(value: any, password: string) {
+    let decrypted = CryptoJS.AES.decrypt(value, password).toString(CryptoJS.enc.Utf8);
+
+    return decrypted;
+  }
+
+}
