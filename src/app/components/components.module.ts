4b3d6fb3650c9d3faa000bd0e7fe8b01e7735b66
alexbilakh <oskar.kamil121@gmail.com>
2023-11-03 10:36:56 -0400

    create link component

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
-import { ButtonComponent } from './button/button.component';
 import { RouterModule } from '@angular/router';
 
+import { ButtonComponent } from './button/button.component';
+import { LinkComponent } from './link/link.component';
+
 @NgModule({
-  declarations: [ButtonComponent],
+  declarations: [ButtonComponent, LinkComponent],
   imports: [CommonModule, IonicModule, RouterModule],
-  exports: [ButtonComponent],
+  exports: [ButtonComponent, LinkComponent],
 })
 export class ComponentsModule {}
