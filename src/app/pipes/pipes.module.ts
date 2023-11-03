ac63620fdebb2aa0bcf6147858b4c93f6925386c
alexbilakh <oskar.kamil121@gmail.com>
2023-11-03 10:51:01 -0400

    create get-data pipe

---
+++ src/app/pipes/pipes.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { GetFormatedDatePipe } from './get-date.pipe';
+
+@NgModule({
+  imports: [],
+  exports: [GetFormatedDatePipe],
+  declarations: [GetFormatedDatePipe],
+  providers: [],
+})
+export class PipesModule {}
