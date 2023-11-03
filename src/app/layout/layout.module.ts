9dbf083dcc516d9a639c166693f71f435847525b
alexbilakh <oskar.kamil121@gmail.com>
2023-11-03 10:44:59 -0400

    create footer

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
+import { FooterComponent } from './footer/footer.component';
 import { HeaderModule } from './header/header.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [FooterComponent],
   imports: [CommonModule, IonicModule, HeaderModule],
-  exports: [HeaderModule],
+  exports: [FooterComponent, HeaderModule],
 })
 export class LayoutModule {}
