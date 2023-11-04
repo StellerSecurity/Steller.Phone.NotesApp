e5114e00425e64e110b3361ed5d73e77e15e31b3
alexbilakh <oskar.kamil121@gmail.com>
2023-11-04 04:58:22 -0400

    change button component

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -7,11 +7,17 @@ import { ButtonComponent } from './button/button.component';
 import { LinkComponent } from './link/link.component';
 import { NoteListComponent } from './note-list/note-list.component';
 import { PipesModule } from '../pipes/pipes.module';
+import { SettingCardModule } from './setting-card/setting-card.module';
 
 @NgModule({
   declarations: [ButtonComponent, LinkComponent, NoteListComponent],
-  imports: [CommonModule, IonicModule, RouterModule, PipesModule],
+  imports: [
+    CommonModule,
+    IonicModule,
+    RouterModule,
+    PipesModule,
+    SettingCardModule,
+  ],
   exports: [ButtonComponent, LinkComponent, NoteListComponent],
 })
-
 export class ComponentsModule {}
