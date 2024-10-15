09494ab241f07157e320324405e5349d8ec62138
ketan <ketan.akbari123@gmail.com>
2024-10-15 21:35:25 +0530

    Added i18n translations to reset password modal

--- src/app/restpass-modal/resetpass-modal.module.ts
+++ src/app/restpass-modal/resetpass-modal.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { ResetPassModalComponent } from './resetpass-modal.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [ResetPassModalComponent],
   imports: [
     CommonModule,
-    IonicModule
+    IonicModule,
+    TranslateModule
   ],
   exports: [ResetPassModalComponent],
 })
