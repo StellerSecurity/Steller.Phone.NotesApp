ae3d998a1f8979e4ee1f936e743c8222d1f8613d
ketan <ketan.akbari123@gmail.com>
2024-10-15 21:29:27 +0530

    Added i18n translations to note locked modal

--- src/app/note-locked-modal/note-locked-modal.module.ts
+++ src/app/note-locked-modal/note-locked-modal.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { NoteLockedModalComponent } from './note-locked-modal.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [NoteLockedModalComponent],
   imports: [
     CommonModule,
-    IonicModule
+    IonicModule,
+    TranslateModule
   ],
   exports: [NoteLockedModalComponent],
 })
