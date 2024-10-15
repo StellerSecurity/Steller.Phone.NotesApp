368c25b9219e0cfa821542bd6f944de17f437a10
ketan <ketan.akbari123@gmail.com>
2024-10-15 20:47:35 +0530

    Added i18n translations to delete note and confirmation modals

--- src/app/confirmation-modal/confirmation-modal.module.ts
+++ src/app/confirmation-modal/confirmation-modal.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { ConfirmationModalComponent } from './confirmation-modal.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [ConfirmationModalComponent],
   imports: [
     CommonModule,
-    IonicModule
+    IonicModule,
+    TranslateModule
   ],
   exports: [ConfirmationModalComponent],
 })
--- src/app/delete-note-modal/delete-note-modal.module.ts
+++ src/app/delete-note-modal/delete-note-modal.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { DeleteNoteModalComponent } from './delete-note-modal.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [DeleteNoteModalComponent],
   imports: [
     CommonModule,
-    IonicModule
+    IonicModule,
+    TranslateModule
   ],
   exports: [DeleteNoteModalComponent],
 })
