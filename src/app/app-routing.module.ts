ca6771c75d31cdb469378478e8d53919972751bc
kalle <info@stellarsecurity.com>
2025-06-05 02:51:55 +0200

    Removed /home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
+    path: '',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
@@ -25,12 +25,7 @@ const routes: Routes = [
   {
     path: 'account/login',
     loadChildren: () => import('./app-settings/app-settings.module').then( m => m.AppSettingsPageModule)
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
+  }
 ];
 
 @NgModule({
