efbd174b9282588f5c3a94243f83de5fd80da184
ketan <ketan.akbari123@gmail.com>
2025-06-22 18:30:09 +0530

    Added Initial UI for the enter pin code

--- src/app/profile/create-account/create-account.component.ts
+++ src/app/profile/create-account/create-account.component.ts
@@ -7,9 +7,11 @@ import { Router } from '@angular/router';
   styleUrls: ['./create-account.component.scss'],
 })
 export class CreateAccountComponent {
-  showPassword = false;
   email = '';
   password = '';
+  showPassword = false;
+  showVerificationSection = false;
+  verificationCode: string[] = ['', '', '', ''];
 
   constructor(private router: Router) {}
 
@@ -18,11 +20,50 @@ export class CreateAccountComponent {
   }
 
   createAccount() {
-    console.log('Creating account with:', this.email, this.password);
-    // Add API integration here
+    // Simulate API call and show verification
+    this.showVerificationSection = true;
   }
 
   goToLogin() {
     this.router.navigate(['/login']);
   }
+
+  resendCode() {
+    console.log('Resend code to:', this.email);
+  }
+
+  changeEmail() {
+    this.showVerificationSection = false;
+  }
+
+  handleCodeInput(event: any, index: number) {
+    const input = event.target as HTMLInputElement;
+    const value = input.value;
+  
+    // Allow only digits
+    if (!/^[0-9]$/.test(value)) {
+      input.value = '';
+      this.verificationCode[index] = '';
+      return;
+    }
+  
+    this.verificationCode[index] = value;
+  
+    // Move to next input
+    const nextInput = input.nextElementSibling as HTMLInputElement;
+    if (nextInput && value) {
+      nextInput.focus();
+    }
+  }
+
+  handleKeyDown(event: KeyboardEvent, index: number) {
+    const input = event.target as HTMLInputElement;
+  
+    // On backspace, move to previous input
+    if (event.key === 'Backspace' && !input.value && input.previousElementSibling) {
+      (input.previousElementSibling as HTMLInputElement).focus();
+    }
+  }
+  
+  
 }
--- src/app/profile/forgot-password/forgot-password.component.ts
+++ src/app/profile/forgot-password/forgot-password.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-forgot-password',
@@ -8,15 +7,48 @@ import { Router } from '@angular/router';
 })
 export class ForgotPasswordComponent {
   email = '';
-
-  constructor(private router: Router) {}
+  showVerification = false;
+  verificationCode: string[] = ['', '', '', ''];
 
   sendCode() {
-    console.log('Sending code to:', this.email);
-    // Trigger backend/email verification logic
+    // Optionally call API to send code to this.email
+    this.showVerification = true;
   }
 
-  backToLogin() {
-    this.router.navigate(['/login']);
+  resendCode() {
+    console.log('Resend code to', this.email);
+  }
+
+  useDifferentEmail() {
+    this.showVerification = false;
+    this.verificationCode = ['', '', '', ''];
+  }
+
+  handleCodeInput(event: any, index: number) {
+    const input = event.target as HTMLInputElement;
+    const value = input.value;
+
+    if (!/^[0-9]$/.test(value)) {
+      input.value = '';
+      this.verificationCode[index] = '';
+      return;
+    }
+
+    this.verificationCode[index] = value;
+
+    if (index < 3) {
+      const nextInput = input.nextElementSibling as HTMLInputElement;
+      if (nextInput) nextInput.focus();
+    } else {
+      const code = this.verificationCode.join('');
+      console.log('Submit code:', code);
+    }
+  }
+
+  handleKeyDown(event: KeyboardEvent, index: number) {
+    const input = event.target as HTMLInputElement;
+    if (event.key === 'Backspace' && !input.value && input.previousElementSibling) {
+      (input.previousElementSibling as HTMLInputElement).focus();
+    }
   }
 }
