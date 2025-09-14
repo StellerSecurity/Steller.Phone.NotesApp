32c92cf3bf558e02a7fea226175ed0ce0f89ff44
ketan <ketan.akbari123@gmail.com>
2025-09-15 00:31:44 +0530

    fixed: remembers the note we had opened last time, so it re-opens automatically

--- src/app/initial/initial.component.ts
+++ src/app/initial/initial.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { AfterViewInit, Component, OnInit } from "@angular/core";
 import { NotesService } from "../services/notes.service";
 import { Router } from "@angular/router";
 
@@ -11,21 +11,22 @@ export class InitialComponent implements OnInit {
   constructor(public noteService: NotesService, private router: Router) {}
 
   ngOnInit() {
-    const notes: any = JSON.parse(this.noteService.getNotes()) || [];
-    if (typeof notes === "string") {
-      return;
-    }
-
+    const notes = JSON.parse(this.noteService.getNotes()) || [];
     const recentOpenedNoteId = localStorage.getItem("recentOpenedNoteId");
 
-    if (recentOpenedNoteId) {
-      const recentNote = notes.find(
+    if (recentOpenedNoteId && notes.length > 0) {
+      const recentNote = notes?.find(
         (note: any) => String(note.id) === String(recentOpenedNoteId)
       );
 
-      if (recentNote && recentNote.id) {
-        this.router.navigate(["/note/" + recentNote.id]);
-      }
+      setTimeout(() => {
+        if (recentNote?.id) {
+          // 🔑 Navigate to note detail
+          this.router.navigate(["/note", recentNote.id]);
+        }
+      }, 300)
+
+     
     }
   }
 }
