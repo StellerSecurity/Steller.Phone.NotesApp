d159a657d0a716aae71b73324df471979d7d73d5
ketan <ketan.akbari123@gmail.com>
2025-09-27 18:36:19 +0530

    Integrated API for login

--- src/app/profile/login/login.component.ts
+++ src/app/profile/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { loginDto } from 'src/app/constants/models/authDto';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -8,19 +11,51 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent {
   showPassword = false;
-  email = '';
-  password = '';
+  loginForm: FormGroup;
+  isSaving = false;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private fb: FormBuilder, private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.initLoginForm();
+  }
+
+  initLoginForm(): void {
+    this.loginForm = this.fb.group({
+      email: ["", [Validators.required, Validators.email]],
+      password: ["", [Validators.required]],
+    });
+  }
 
   togglePasswordVisibility() {
     this.showPassword = !this.showPassword;
   }
 
   login() {
-    // Implement your login logic
-    console.log('Email:', this.email);
-    console.log('Password:', this.password);
+    if (this.loginForm.valid) {
+      this.isSaving = true;
+      const loginObj: loginDto = {
+        username: this.loginForm.get("email")?.value,
+        password: this.loginForm.get("password")?.value,
+      };
+
+      this.authService.loginHandling(loginObj).subscribe({
+        next: (response:any) => {
+          this.isSaving = false;
+          if (response.response_code == 200) {
+            // this.localStorageService.storeToLocalStorage("ssToken", response.token);
+            // this.toastrService.success(this.translatorService.allTranslations.loggedInSuccessfully);
+            this.router.navigate(["/"]);
+          } else {
+            // this.toastrService.error(response.response_message);
+          }
+        },
+        error: (error) => {
+          this.isSaving = false;
+          // this.toastrService.error(error?.error?.message);
+        },
+      });
+    }
   }
 
   navigateToRegister() {
