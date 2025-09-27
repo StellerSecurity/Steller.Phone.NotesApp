f71a2982e4cb776da32c97a5d69242986083da29
ketan <ketan.akbari123@gmail.com>
2025-09-27 20:16:30 +0530

    Integrated API for reset password

--- src/app/profile/create-new-password/create-new-password.component.ts
+++ src/app/profile/create-new-password/create-new-password.component.ts
@@ -1,18 +1,66 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-create-new-password',
   templateUrl: './create-new-password.component.html',
   styleUrls: ['./create-new-password.component.scss'],
 })
-export class CreateNewPasswordComponent {
+export class CreateNewPasswordComponent implements OnInit, OnDestroy {
   newPassword = '';
   confirmPassword = '';
   showNewPassword = false;
   showConfirmPassword = false;
+  passwordForm: FormGroup;
+  isSaving = false;
+  confirmationCode = '';
+  email = '';
+  subscriptions: Subscription[] = [];
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private fb: FormBuilder,
+    private authService: AuthService, private activatedRoute: ActivatedRoute) {}
+
+    ngOnInit(): void {
+      this.initPasswordForm();
+      this.getQueryParams();
+    }
+
+    getQueryParams(): void {
+      this.subscriptions.push(
+        this.activatedRoute.queryParamMap.subscribe((queryParams: any) => {
+          if (queryParams.has("confirmationCode")) {
+            this.confirmationCode = queryParams.get("confirmationCode");
+          }
+          if (queryParams.has("email")) {
+            this.email = queryParams.get("email");
+          }
+        })
+      );
+    }
+  
+    initPasswordForm(): void {
+      this.passwordForm = this.fb.group(
+        {
+          password: ["", [Validators.required, Validators.minLength(6)]],
+          confirmPassword: ["", Validators.required],
+        },
+        { validator: this.passwordMatchValidator }
+      );
+    }
+  
+    passwordMatchValidator(form: FormGroup) {
+      const password = form.get("password")?.value;
+      const confirmPassword = form.get("confirmPassword")?.value;
+  
+      if (password !== confirmPassword) {
+        form.get("confirmPassword")?.setErrors({ mismatch: true });
+      } else {
+        form.get("confirmPassword")?.setErrors(null);
+      }
+    }
 
   toggleNewPasswordVisibility() {
     this.showNewPassword = !this.showNewPassword;
@@ -23,16 +71,36 @@ export class CreateNewPasswordComponent {
   }
 
   confirm() {
-    if (this.newPassword !== this.confirmPassword) {
-      console.error('Passwords do not match.');
-      return;
+    if (this.passwordForm.valid) {
+      this.isSaving = true;
+      this.authService
+        .resetPassword({
+          new_password: this.passwordForm.get("password")?.value,
+          confirmation_code: this.confirmationCode,
+          email: this.email,
+        })
+        .subscribe({
+          next: (response: any) => {
+            this.isSaving = false;
+            if (response.response_code == 200) {
+              this.backToLogin();
+              
+            } else {
+              // this.toastrService.error(error?.error?.message);
+            }
+          },
+          error: (error: any) => {
+            this.isSaving = false;
+          },
+        });
     }
-
-    console.log('New password confirmed:', this.newPassword);
-    // Send to backend API
   }
 
   backToLogin() {
     this.router.navigate(['/profile/login']);
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(sub => sub.unsubscribe());
   }
 }
