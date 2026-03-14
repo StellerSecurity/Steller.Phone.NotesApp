64057404bfae6fdec3fc77414ae78eff2ea595e5
kalle <info@stellarsecurity.com>
2026-03-14 19:00:21 +0100

    upgraded capactior, ios etc.

--- android/app/src/main/java/Steller/Phone/NotesApp/MainActivity.java
+++ android/app/src/main/java/Steller/Phone/NotesApp/MainActivity.java
@@ -1,5 +1,27 @@
 package Steller.Phone.NotesApp;
 
+import android.os.Bundle;
+import android.view.WindowManager;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        enableSecureScreen();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        enableSecureScreen();
+    }
+
+    private void enableSecureScreen() {
+        getWindow().setFlags(
+            WindowManager.LayoutParams.FLAG_SECURE,
+            WindowManager.LayoutParams.FLAG_SECURE
+        );
+    }
+}
